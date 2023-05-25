48c19c3e3fe7e4fe3dc50958dcc4e6131ae5f8bf
bun <container@172.17.0.69>
2023-05-25 10:37:50 +0000

    big cock

--- project4-kvs-cache/kvs_clock (copy) before cock advancement.c
+++ project4-kvs-cache/kvs_clock (copy) before cock advancement.c
@@ -76,8 +76,7 @@ int kvs_clock_set(kvs_clock_t* kvs_clock, const char* key, const char* value) {
     kvs_clock->keys[kvs_clock->cursor] = strdup(key);
     kvs_clock->values[kvs_clock->cursor] = strdup(value);
     kvs_clock->dirty[kvs_clock->cursor] = true; // mark as dirty
-    //kvs_clock->cursor = (kvs_clock->cursor + 1) % kvs_clock->capacity;
-    kvs_clock->ref_bits[kvs_clock->cursor] = 1; // set the reference bit to 1
+    kvs_clock->cursor = (kvs_clock->cursor + 1) % kvs_clock->capacity;
   }
   
   return 0;
@@ -113,7 +112,7 @@ int kvs_clock_get(kvs_clock_t* kvs_clock, const char* key, char* value) {
       kvs_clock->dirty[kvs_clock->cursor] = false; // new entry is not dirty
       kvs_clock->ref_bits[kvs_clock->cursor] = 1; // set the reference bit to 1
       
-      //kvs_clock->cursor = (kvs_clock->cursor + 1) % kvs_clock->capacity;
+      kvs_clock->cursor = (kvs_clock->cursor + 1) % kvs_clock->capacity;
     } else {
       kvs_clock->keys[kvs_clock->size] = strdup(key);
       kvs_clock->values[kvs_clock->size] = strdup(value);
